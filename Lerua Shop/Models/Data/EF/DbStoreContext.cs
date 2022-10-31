3c6f688ab7015bc99f43c34fd6347f2f75b3a164
Samitova Vika <samitova@gmail.com>
2022-10-31 21:29:22 +0200

    Create DbStoreContext - EF Context

---
+++ Lerua Shop/Models/Data/EF/DbStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Lerua_Shop.Models.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lerua_Shop.Models.Data.EF
+{
+    public class DbStoreContext : DbContext
+    {
+        public DbSet<PageDTO> Pages { get; set; }
+
+        public DbStoreContext() : base("name=DbConnection")
+        { }
+    }
+}
