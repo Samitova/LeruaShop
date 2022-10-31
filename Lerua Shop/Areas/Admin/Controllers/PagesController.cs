36484b201ceaf24b74d5abe0f54240bf658c089a
Samitova Vika <samitova@gmail.com>
2022-10-31 22:45:48 +0200

    Add PageController and implement Index method and view

---
+++ Lerua Shop/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,24 @@
+﻿using Lerua_Shop.Models.Data.Repository;
+using Lerua_Shop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lerua_Shop.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PagesController : Controller
+    {
+        private readonly GeneralRepository _repository = GeneralRepository.GetInstance();
+
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pages = _repository.PagesRepository.GetAll(orderBy: q => q.OrderBy(s => s.Sorting))
+                                                            .Select(x => new PageVM(x)).ToList();
+            return View(pages);           
+        }
+    }
+}
