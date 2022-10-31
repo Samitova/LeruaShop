ef3611a6e19ef05efd9e8e4d2278873b9a1121a5
Samitova Vika <samitova@gmail.com>
2022-10-31 21:36:45 +0200

    Create generic interface IRepository<T>

---
+++ Lerua Shop/Models/Data/Repository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lerua_Shop.Models.Data.Repository
+{
+    internal interface IRepository<T>
+    {
+        int Add(T entity);
+        int AddRange(IList<T> entities);
+        int Edit(T entity);
+        int Delete(int id, byte[] timestamp);
+        int Delete(T entity);
+        T GetOne(int? id);
+        T GetOne(Expression<Func<T, bool>> filter);
+        bool Any(Expression<Func<T, bool>> filter);
+
+        List<T> GetAll(Expression<Func<T, bool>> filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, string includeProperties);
+        List<T> ExecuteQuery(string sql);
+        List<T> ExecuteQuery(string sql, object[] sqlParametersObjects);
+    }
+}
