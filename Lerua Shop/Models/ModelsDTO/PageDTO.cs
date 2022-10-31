d906de6da779e81783219225076dc241c4c7921a
Samitova Vika <samitova@gmail.com>
2022-10-31 21:25:38 +0200

    Create tblPages , PageDTO and PageVM

---
+++ Lerua Shop/Models/ModelsDTO/PageDTO.cs
@@ -0,0 +1,19 @@
+﻿using Lerua_Shop.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Lerua_Shop.Models.ModelsDTO
+{
+    [Table("tblPages")]
+    public class PageDTO : EntityBase
+    {
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
---
+++ Lerua Shop/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,42 @@
+﻿using Lerua_Shop.Models.Base;
+using Lerua_Shop.Models.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace Lerua_Shop.Models.ViewModels.Pages
+{
+    public class PageVM : EntityBase
+    {
+        public PageVM()
+        { }
+        public PageVM(PageDTO pages)
+        {
+            Id = pages.Id;
+            Title = pages.Title;
+            Slug = pages.Slug.Replace(" ", "-").ToLower();
+            Body = pages.Body;
+            Sorting = pages.Sorting;
+            HasSidebar = pages.HasSidebar;
+            Timestamp = pages.Timestamp;
+        }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        [AllowHtml]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        [Display(Name = "Sidebar")]
+        public bool HasSidebar { get; set; }
+    }
+}
