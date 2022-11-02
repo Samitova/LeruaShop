f20475e4d1ac1cf474c2e5985fe952c6f73c634d
Samitova Vika <samitova@gmail.com>
2022-11-02 21:48:51 +0200

    Application_AuthenticateRequest was added

--- Lerua Shop/Global.asax.cs
+++ Lerua Shop/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Lerua_Shop.Models.Data.Repository;
+using Lerua_Shop.Models.ModelsDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +19,28 @@ namespace Lerua_Shop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            GeneralRepository _repository = GeneralRepository.GetInstance();
+            if (User == null)
+                return;
+            string userName = Context.User.Identity.Name;
+
+            string[] roles = null;
+
+            UserDTO userDTO = _repository.UsersRepository.GetOne(x => x.UserName == User.Identity.Name);
+            if (userDTO == null)
+                return;
+
+            roles = _repository.UserRolesRepository.GetAll(filter: x => x.Id == userDTO.Id).Select(x => x.Role.Name).ToArray();
+
+            IIdentity userIdentity = new GenericIdentity(userName);
+            IPrincipal newUserObject = new GenericPrincipal(userIdentity, roles);
+
+            Context.User = newUserObject;
+
         }
     }
 }
